ead7abcae9c01ba4e6ca9f7c7f16f3a9bf0650ee
Baptiste <b.menard@student.isartdigital.com>
2016-10-18 15:06:26 +0200

    FPS player

---
+++ ToysWarProto/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+	void Start()
+	{
+		Destroy(gameObject, 1);
+	}
+
+	void Update ()
+	{
+		transform.position += transform.up * Time.deltaTime * 20;
+	}
+
+}
+
---
+++ ToysWarProto/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject bulletPrefab;
+
+
+	void Update ()
+	{
+		Shoot();
+	}
+
+	private void Shoot()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Instantiate(bulletPrefab, transform.position, transform.rotation);
+		}
+	}
+
+}
+
